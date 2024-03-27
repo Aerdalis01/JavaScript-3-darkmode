149a65477398a2d01736b3e77d0f10d773ca253f
Aerdalis01 <caillonaudric@hotmail.fr>
2024-03-27 17:31:25 +0100

    Ajout de js

--- script.js
+++ script.js
@@ -16,9 +16,42 @@ console.log(title);
 
 
 switchBox.addEventListener('click', function(){
-  console.log("div cliqué");
+  //console.log("div cliqué");
 
-  btn.style.left= "60px"
+btn.classList.toggle('btn-change');
+icone.classList.toggle('icone-change');
+icone.classList.toggle('fa-sun');
+icone.classList.toggle('fa-moon');
+container.classList.toggle('container-change');
+switchBox.classList.toggle('switch-change');
+if(title.innerText === "LIGHT MODE") {
+  title.innerText = "DARK MODE";
+  title.style.color = "white";
+} else {
+  title.innerText = "LIGHT MODE";
+  title.style.color = "black";
+}
 
+});
+
+const bodyFont = document.querySelector('.hero');
+console.log(bodyFont);
+
+const buttonPlus = document.querySelector('.button-plus');
+console.log(buttonPlus);
+
+const buttonMoins = document.querySelector('.button-moins');
+console.log(buttonMoins);
+
+var t;			
+	t = 1;
+
+	function changerTaille(modif) {
+		t = t + modif;
+		document.getElementsByTagName("body")[0].style.fontSize = t + "em";		
+	} 	
+  
+buttonPlus.addEventListener('click', function(){
+  bodyFont.style.fontSize = "0.5rem" ; 
 });
 
