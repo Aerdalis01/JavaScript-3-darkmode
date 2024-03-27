9c154881278cde32d4a0b6eb3a453f31d183bfc6
Aerdalis01 <caillonaudric@hotmail.fr>
2024-03-27 16:34:38 +0100

    Mis en place index et style.css

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("connecté!")
+const switchBox = document.querySelector('.switch');
+console.log(switchBox);
+
+const btn = document.querySelector('.btn');
+console.log(btn);
+
+
+const icone = document.querySelector('i');
+console.log(icone);
+
+const container = document.querySelector('.container');
+console.log(container);
+
+const title = document.querySelector('h1');
+console.log(title);
+
+
+switchBox.addEventListener('click', function(){
+  console.log("div cliqué");
+
+  btn.style.left= "60px"
+
+});
+
